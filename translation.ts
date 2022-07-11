6ca3afbc259a6c3a0ec1a416dd6ddece5da5d748
Abduvokhid <abduvohid1996@gmail.com>
2022-07-11 14:37:11 +0500

    Finished it

--- translation.ts
+++ translation.ts
@@ -32,7 +32,7 @@ const translation = {
         list: sponsors
       },
       about: {
-        content: '<b>@InlineMemoBot@</b> — helps to create posts to send them later to any chat without re-creating it. Bot supports text, photo, video, audio, voice, GIF and document. Just type <code>@InlineMemoBot</code> and post ID in any chat to send created a post.\n\nContact me with suggestions and feedback: @Developer616'
+        content: '<b>@InlineMemoBot</b> — helps to create posts to send them later to any chat without re-creating it. Bot supports text, photo, video, audio, voice, GIF and document. Just type <code>@InlineMemoBot</code> and post ID in any chat to send created a post.\n\nContact me with suggestions and feedback: @Developer616'
       },
       create_post: {
         content: '<b>Now send the content of the post.</b>\n\n<i>It might be one of text, photo, audio, video, document, animation/GIF or voice. All other types of content is not accepted.</i>\n\nInclude captions if you need one. Already formatted text/caption is accepted or you can send text/caption with HTML tags.',
